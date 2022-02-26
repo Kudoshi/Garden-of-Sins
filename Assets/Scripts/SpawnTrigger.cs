e8cd590310df3905ed8ed9d81d7effc1f6c14111
Kudoshi <brendenguan@live.com>
2022-02-27 07:55:55 +0800

    Imported and animated all sprites

--- Assets/Scripts/SpawnTrigger.cs
+++ Assets/Scripts/SpawnTrigger.cs
@@ -11,12 +11,20 @@ using UnityEngine;
 public class SpawnTrigger : MonoBehaviour
 {
     public SpawnManager spawnManager;
+    public AnimationController animator;
 
+    private void Awake()
+    {
+        //animator.ChangeAnimationState("FireOff");
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             spawnManager.UpdateSpawnPoint(transform);
+            animator.ChangeAnimationState("FireOn");
+            GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 }
